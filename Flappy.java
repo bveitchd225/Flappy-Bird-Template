5a2702ad07932ea889ebe3ea52de379e636686a1
Brett Veitch <bveitch@glenbrook225.org>
2026-09-24 11:16:29 -0500

    Starter Code

---
+++ Flappy.java
@@ -0,0 +1,24 @@
+import java.awt.*;
+import gbs.game.*;
+
+public class Flappy extends GBSGame {
+
+    public Flappy() {
+
+    }
+
+    public void update(double dt) {
+        
+    }
+
+    public void draw(Graphics g) {
+        
+    }
+
+    public static void main(String[] args) {
+        Flappy game = new Flappy();
+
+        
+        game.createWindow();
+    }   
+}
